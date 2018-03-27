13c9309a8ce5e1bc16f004938b0af1c2e53ce96e
Adam Wolf <adamwolf@feelslikeburning.com>
2018-03-27 09:19:39 -0500

    build.sh now detects the number of logical cores available.

--- build.sh
+++ build.sh
@@ -6,7 +6,9 @@ set -x
 mkdir -p build
 cd build
 
+NUM_CORES=`sysctl -n hw.ncpu`
+
 export PATH=$PATH:/usr/local/opt/gettext/bin
 cmake -DMACOS_MIN_VERSION=`sw_vers -productVersion | cut -d. -f1-2` ../kicad-mac-builder
-make -j4 || make
+make -j$NUM_CORES || make
 echo "build succeeded."
