ba88cedf2a9e7f5ee9e0133b10f19a8bca5f1ff9
Adam Wolf <adamwolf@feelslikeburning.com>
2018-04-05 10:51:51 -0500

    Setup ci to build all dmgs. (#58)
    Added pending notifier.
        Added linting to Jenkins.  Made linting fixes.
        Removed old scripts.
        Shell style updates.
        Removed the intermediary support and extras directory.
        Using, you know, proper shell syntax and all.
        Added unified packaging.  Removed packaging as default targets.
        Revamped package-*.sh into a common piece.
        build.sh now detects the number of logical cores available.
        Updated Vagrant and Jenkinsfiles.
        Moved 10.11 scripts to common area, added 10.12 ci area.
        Added 10.11 CI stuff in a VM.

--- ci/src/run.sh
+++ ci/src/run.sh
@@ -6,9 +6,16 @@ set -x
 export PATH="${PATH}":/usr/local/opt/gettext/bin
 NUM_CORES=$(sysctl -n hw.ncpu)
 
+cd /vagrant
+
+#if [ -e build ] ; then
+#  rm -r build
+#fi
+
 mkdir -p build
 cd build
 
+export PATH=$PATH:/usr/local/opt/gettext/bin
 cmake -DMACOS_MIN_VERSION="$(sw_vers -productVersion | cut -d. -f1-2)" ../kicad-mac-builder
-make -j"${NUM_CORES}" "$@"
+make -j"${NUM_CORES}" package-kicad-nightly package-extras package-kicad-unified
 echo "build succeeded."
