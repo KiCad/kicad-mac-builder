c3fa97c10be8a5384f203e94d67792888c5e8b75
Adam Wolf <adamwolf@feelslikeburning.com>
2018-04-22 14:43:24 -0500

    Initial work on using a Python built as a Framework.

--- kicad-mac-builder/python.cmake
+++ kicad-mac-builder/python.cmake
@@ -8,8 +8,25 @@ ExternalProject_Add(
         UPDATE_COMMAND      ""
         PATCH_COMMAND       ""
         CONFIGURE_COMMAND MACOSX_DEPLOYMENT_TARGET=${MACOS_MIN_VERSION} ./configure
-                    --prefix=${PYTHON_INSTALL_DIR}
-                    --enable-shared
+                    --enable-framework=${PYTHON_INSTALL_DIR}
         BUILD_COMMAND make
         BUILD_IN_SOURCE 1
+        INSTALL_COMMAND make frameworkinstallframework
+)
+
+#include(BundleUtilities)
+#fixup_bundle( ${PYTHON_INSTALL_DIR}/Python.framework/Versions/2.7/Resources/Python.app "" "")
+# results in dyld: Library not loaded: @executable_path/../Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python
+#  Referenced from: /Users/wolf/wnl/kicad/kicad-mac-builder/build/python-dest/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python
+#  Reason: no suitable image found.  Did find:
+#	/System/Library/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python: not a dylib
+#	/System/Library/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python: not a dylib
+#[1]    52659 abort
+
+ExternalProject_Add_Step(
+        python
+        fixup
+        COMMENT "Fixing up the Python framework"
+        DEPENDEES install
+        COMMAND install_name_tool -change "${PYTHON_INSTALL_DIR}/Python.framework/Versions/2.7/Python" "@loader_path/../../../../Python" ${PYTHON_INSTALL_DIR}/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python
 )
