f6b0e2c6fd925d89d2aa8d0a757a68e6204caa91
Adam Wolf <adamwolf@feelslikeburning.com>
2018-04-22 20:41:51 -0500

    Added comments.  Worked around python install race condition.

--- kicad-mac-builder/python.cmake
+++ kicad-mac-builder/python.cmake
@@ -11,17 +11,11 @@ ExternalProject_Add(
                     --enable-framework=${PYTHON_INSTALL_DIR}
         BUILD_COMMAND make
         BUILD_IN_SOURCE 1
-        INSTALL_COMMAND make frameworkinstallframework
+        INSTALL_COMMAND make -j1 frameworkinstallframework
 )
 
-#include(BundleUtilities)
-#fixup_bundle( ${PYTHON_INSTALL_DIR}/Python.framework/Versions/2.7/Resources/Python.app "" "")
-# results in dyld: Library not loaded: @executable_path/../Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python
-#  Referenced from: /Users/wolf/wnl/kicad/kicad-mac-builder/build/python-dest/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python
-#  Reason: no suitable image found.  Did find:
-#	/System/Library/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python: not a dylib
-#	/System/Library/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python: not a dylib
-#[1]    52659 abort
+# For some reason, we need to do the install step with one core only.  I haven't found or created an upstream bug, but other people have run into something similar.
+# https://github.com/Homebrew/legacy-homebrew/issues/429
 
 ExternalProject_Add_Step(
         python
@@ -30,3 +24,14 @@ ExternalProject_Add_Step(
         DEPENDEES install
         COMMAND install_name_tool -change "${PYTHON_INSTALL_DIR}/Python.framework/Versions/2.7/Python" "@loader_path/../../../../Python" ${PYTHON_INSTALL_DIR}/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python
 )
+
+# I tried to use fixup_bundle to do that change from above, but it tried to do too much.  I did:
+#include(BundleUtilities)
+#fixup_bundle( ${PYTHON_INSTALL_DIR}/Python.framework/Versions/2.7/Resources/Python.app "" "")
+# results in dyld: Library not loaded: @executable_path/../Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python
+#  Referenced from: /Users/wolf/wnl/kicad/kicad-mac-builder/build/python-dest/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python
+#  Reason: no suitable image found.  Did find:
+#	/System/Library/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python: not a dylib
+#	/System/Library/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python: not a dylib
+#[1]    52659 abort
+# If someone can get BundleUtilities to replace the manual install_name_tool step I'd love to see it!
