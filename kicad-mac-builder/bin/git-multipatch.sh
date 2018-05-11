39e3f7054ad99188118ef3ddfb2f432fb568e70d
Adam Wolf <adamwolf@feelslikeburning.com>
2018-05-11 08:56:51 -0500

    Fixed git multipatch.  Added a kicad patch to help kicad-mac-builder work on cmake 3.6.2.

--- kicad-mac-builder/bin/git-multipatch.sh
+++ kicad-mac-builder/bin/git-multipatch.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
 if [ $# -ne 1 ]; then
-    echo "Bad number of arguments, expects glob of patches"
+    echo "Bad number of arguments, expects one glob"
     exit 1
 fi
 
-
 for file in $1; do
-    if [ ! -e "$file" ]; then
-        echo "Unable to find files at $1"
+    echo "$file"
+    if [ ! -e "${file}" ]; then
+        echo "Unable to find file at $file"
         exit 1
     fi
 
-    git am "$file"
+    git am "${file}"
     EXITCODE=$?
     if [ $EXITCODE -ne 0 ]; then
-        echo "Problem while applying patch $1"
+        echo "Problem while applying patch ${file}"
         exit $EXITCODE
     fi
 
     git status | grep "^You are in the middle of an am session" > /dev/null
     EXITCODE=$?
-    if [ $EXITCODE -eq 1 ]; then
-       exit 0
-    else
+    if [ $EXITCODE -ne 1 ]; then
        echo "Something is wrong in your build/kicad/src/kicad directory. Exiting."
        exit 1
     fi
