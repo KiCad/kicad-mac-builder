05e321c5fb486efa28ad59714e4a5e804700a6eb
Adam Wolf <adamwolf@feelslikeburning.com>
2018-05-14 20:51:25 -0500

    Adjust CI bootstrap for Cmake 3.6.2 (#118)
    * Remove cmake from the second install.
    * Pass through build.sh arguments.

--- ci/src/bootstrap.sh
+++ ci/src/bootstrap.sh
@@ -18,4 +18,4 @@ echo "Installing cmake 3.6.2 for testing"
 brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/eaf0d6c78c8d49ffeaa158eb307101f034944795/Formula/cmake.rb
 cmake --version
 echo "Installing some dependencies"
-brew install cmake swig glew glm cairo boost doxygen gettext wget brewsci/science/oce bison libtool autoconf automake
+brew install swig glew glm cairo boost doxygen gettext wget brewsci/science/oce bison libtool autoconf automake # cmake
