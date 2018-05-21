4c7815f83e1f524c77b2b0a17a7945c94a0e879f
Adam Wolf <adamwolf@feelslikeburning.com>
2018-05-21 08:56:08 -0500

    verify-cli-python.sh now tries to fixup the rpath.
    This is to fix a weird bug I am seeing on only one 10.11 machine.  It does not appear to be cmake version dependent, and it does not happen 100% of the time, but when it does happen, the rpaths and everything in python-dest are just fine, but *one* rpath in kicad.app is broken.
    
    I am adding logging and will be looking for a deeper cause, but this is a temporary workaround.

--- kicad-mac-builder/bin/verify-cli-python.sh
+++ kicad-mac-builder/bin/verify-cli-python.sh
@@ -4,6 +4,19 @@ if [ ! -e "$1" ]; then
   exit 1
 fi
 
+# I do not understand why this top check is ever needed.  After July 2018, I should verify in the logs to see if this is still seen, and if not, remove it.
+
+otool -L $1 | grep '@executable_path/../../Contents/MacOS/Python.framework/Versions/2.7/Python' > /dev/null
+if [ "$?" -ne 1 ]; then
+        echo "$1 appears to be incorrectly setup.  There is a bug where this does not occur in python-dest but does in kicad-dest, and as I am figuring it out, I am working around it here."
+        echo "Trying to fix $1"
+        install_name_tool -change @executable_path/../../Contents/MacOS/Python.framework/Versions/2.7/Python "@rpath/Python.framework/Python" "$1"
+        otool -L $1 | grep '@executable_path/../../Contents/MacOS/Python.framework/Versions/2.7/Python' > /dev/null
+        if [ "$? -eq 1 ]; then
+            echo "It appears to be fixed."
+        fi
+fi
+
 DYLD_PRINT_LIBRARIES=1 "$1" -c 'print "Hello world."' 2>&1 | grep /System/Library/Frameworks/Python.framework
 if [ "$?" -ne 1 ]; then
 	echo "$1 appears to call the System Python framework.  DYLD_PRINT_LIBRARIES=1 \"$1\" may help you debug the issue."
