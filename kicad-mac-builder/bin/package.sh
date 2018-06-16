51dd003075d6f766920281731353680f5046fdd1
Adam Wolf <adamwolf@feelslikeburning.com>
2018-06-15 22:28:20 -0500

    Moving demos out of kicad directory in package.

--- kicad-mac-builder/bin/package.sh
+++ kicad-mac-builder/bin/package.sh
@@ -144,6 +144,10 @@ case "${PACKAGE_TYPE}" in
         mkdir -p "${MOUNTPOINT}"/KiCad
         rsync -al "${KICAD_INSTALL_DIR}"/* "${MOUNTPOINT}"/KiCad/. # IMPORTANT: must preserve symlinks
         mkdir -p "${MOUNTPOINT}"/kicad
+        echo "Moving demos"
+        if [ -e "${MOUNTPOINT}"/kicad/kicad/demos ]; then
+            mv "${MOUNTPOINT}"/kicad/kicad/demos "${MOUNTPOINT}"/kicad/demos
+        fi
         echo "Copying docs"
         cp -r "${CMAKE_BINARY_DIR}"/docs/kicad-doc-HEAD/share/doc/kicad/help "${MOUNTPOINT}"/kicad/
         echo "Copying translations"
@@ -166,6 +170,10 @@ case "${PACKAGE_TYPE}" in
         mkdir -p "${MOUNTPOINT}"/KiCad
         rsync -al "${KICAD_INSTALL_DIR}"/* "${MOUNTPOINT}"/KiCad/. # IMPORTANT: must preserve symlinks
         mkdir -p "${MOUNTPOINT}"/kicad
+        echo "Moving demos"
+        if [ -e "${MOUNTPOINT}"/kicad/kicad/demos ]; then
+            mv "${MOUNTPOINT}"/kicad/kicad/demos "${MOUNTPOINT}"/kicad/demos
+        fi
         echo "Copying docs"
         cp -r "${CMAKE_BINARY_DIR}"/docs/kicad-doc-HEAD/share/doc/kicad/help "${MOUNTPOINT}"/kicad/
         echo "Copying translations"
