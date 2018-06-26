d69f9d7999897773c76ed931ffd49b242249093c
Adam Wolf <adamwolf@feelslikeburning.com>
2018-06-25 21:30:00 -0500

    Comment out GIT_PROGRESS on packages3d because it doesn't work on the official KiCad build machine :(

--- kicad-mac-builder/packages3d.cmake
+++ kicad-mac-builder/packages3d.cmake
@@ -2,7 +2,7 @@ ExternalProject_Add(
         packages3d
         PREFIX  packages3d
         GIT_REPOSITORY https://github.com/KiCad/kicad-packages3D.git
-        GIT_PROGRESS 1
+        #GIT_PROGRESS 1 #TODO uncomment when the official KiCad Mac gets updated...
         PATCH_COMMAND ""
         CMAKE_ARGS "-DCMAKE_INSTALL_PREFIX=<BINARY_DIR>/output"
 )
