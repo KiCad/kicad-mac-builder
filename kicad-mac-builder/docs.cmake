380a0e327ca297630e60245f88775c58366f3df2
Adam Wolf <adamwolf@feelslikeburning.com>
2018-07-05 09:20:20 -0500

    Modifying docs downloads.

--- kicad-mac-builder/docs.cmake
+++ kicad-mac-builder/docs.cmake
@@ -3,8 +3,8 @@ set( DOCS_OUTPUT_DIR ${CMAKE_BINARY_DIR}/docs/help )
 ExternalProject_Add(
         docs
         PREFIX  docs
-        UPDATE_COMMAND mkdir -p ${CMAKE_BINARY_DIR}/docs/src
-        COMMAND curl ${DOCS_TARBALL_URL} -o ${CMAKE_BINARY_DIR}/docs/src/docs.tar.gz
+        DOWNLOAD_COMMAND mkdir -p ${CMAKE_BINARY_DIR}/docs/src
+        UPDATE_COMMAND curl ${DOCS_TARBALL_URL} -o ${CMAKE_BINARY_DIR}/docs/src/docs.tar.gz
         CONFIGURE_COMMAND ""
         BUILD_COMMAND tar -xf ${CMAKE_BINARY_DIR}/docs/src/docs.tar.gz -C <INSTALL_DIR>
         INSTALL_COMMAND ""
