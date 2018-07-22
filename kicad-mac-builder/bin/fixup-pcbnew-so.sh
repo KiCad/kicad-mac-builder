5f3d70f045526b32519773f0bd1e739fc7083387
Adam Wolf <adamwolf@feelslikeburning.com>
2018-07-22 07:59:45 -0500

    Be more verbose about what fixup-pcbnew-so is doing.  Exit fixup-pcbnew-so on a real error.  Squelch the 'duplicate rpath' messages.

--- kicad-mac-builder/bin/fixup-pcbnew-so.sh
+++ kicad-mac-builder/bin/fixup-pcbnew-so.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-set -x
+set -e
+
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+     # While $SOURCE is a symlink, resolve it
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          # If $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+     echo "$DIR"
+}
+
+SCRIPT_DIR=$(get_script_dir)
 
 # Many thanks to metacollin.
 
@@ -20,9 +35,13 @@ for file in *.dylib; do
     fixup_libs=($(otool -L $file | grep -o "@executable_path.*.dylib"));
     libs=($(echo ${fixup_libs[@]} | grep -o "[^/]*dylib"));
     for ((i=0;i<${#fixup_libs[@]};++i)); do
+        echo "Changing ${fixup_libs[i]} to @rpath/${libs[i]} in $file"
         install_name_tool -change "${fixup_libs[i]}" "@rpath/${libs[i]}" $file;
     done;
-    install_name_tool -add_rpath @loader_path/../.. -add_rpath @executable_path/../Frameworks $file;
+    echo "Adding rpaths of @loader_path/../../ and @executable_path/../Frameworks to $file"
+    #install_name_tool -add_rpath @loader_path/../.. -add_rpath @executable_path/../Frameworks $file;
+    $SCRIPT_DIR/add-rpath.sh @loader_path/../.. $file;
+    $SCRIPT_DIR/add-rpath.sh @executable_path/../Frameworks $file;
 done;
 
 cd python/site-packages
@@ -30,9 +49,13 @@ cd python/site-packages
 fixup_libs=($(otool -L _pcbnew.so | grep -o "@executable_path.*.dylib"));
 libs=($(echo ${fixup_libs[@]} | grep -o "[^/]*dylib"));
 for ((i=0;i<${#fixup_libs[@]};++i)); do
+    echo "Changing ${fixup_libs[i]} to @rpath/${libs[i]} in _pcbnew.so"
     install_name_tool -change "${fixup_libs[i]}" "@rpath/${libs[i]}" _pcbnew.so;
 done
 
-install_name_tool -add_rpath @loader_path/../.. -add_rpath @executable_path/../Frameworks _pcbnew.so
+echo "Adding rpaths of @loader_path/../../ and @executable_path/../Frameworks to _pcbnew.so"
+#install_name_tool -add_rpath @loader_path/../.. -add_rpath @executable_path/../Frameworks _pcbnew.so
+$SCRIPT_DIR/add-rpath.sh @loader_path/../.. _pcbnew.so
+$SCRIPT_DIR/add-rpath.sh @executable_path/../Frameworks _pcbnew.so
 
 echo "done"
