f6e8539651b4dd4f6678e5f97ab1d2439a21f1bc
Adam Wolf <adamwolf@feelslikeburning.com>
2018-08-04 08:14:34 -0500

    Revert "Folder icon made fancy in DMG (nightly and unified targets only)"
    This reverts commit aae200c2a0e0f9e113464dfe156458f25172cf1e.

--- kicad-mac-builder/bin/package.sh
+++ kicad-mac-builder/bin/package.sh
@@ -45,11 +45,6 @@ fixup_and_cleanup()
     # Rehide the background file
     SetFile -a V "${MOUNTPOINT}"/background.png
 
-    # set the icon of the Kicad folder
-    if [ "${PACKAGE_TYPE}" != "extras" ]; then
-        python -c "import Cocoa ; import sys ; Cocoa.NSWorkspace.sharedWorkspace().setIcon_forFile_options_(Cocoa.NSImage.alloc().initWithContentsOfFile_(sys.argv[1].decode('utf-8')), sys.argv[2].decode('utf-8'), 0) or sys.exit(\"Unable to set file icon\");" "${PACKAGING_DIR}"/folder-icon.png "${MOUNTPOINT}"/KiCad
-    fi
-
     hdiutil detach "${MOUNTPOINT}"
     rm -r "${MOUNTPOINT}"
 
