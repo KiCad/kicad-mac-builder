84a7a3b3119bd791f429c8a6a0d31945becbf18d
Nick Østergaard <oe.nick@gmail.com>
2019-01-24 23:05:09 +0100

    Add -L option to curl for docs
    This ensures that we can move the URL dor the doc tarball around as we
    see fit... Dash big L will make it follow 3XX codes.

--- kicad-mac-builder/docs.cmake
+++ kicad-mac-builder/docs.cmake
@@ -4,7 +4,7 @@ ExternalProject_Add(
         docs
         PREFIX  docs
         DOWNLOAD_COMMAND mkdir -p ${CMAKE_BINARY_DIR}/docs/src
-        UPDATE_COMMAND curl ${DOCS_TARBALL_URL} -o ${CMAKE_BINARY_DIR}/docs/src/docs.tar.gz
+        UPDATE_COMMAND curl -L ${DOCS_TARBALL_URL} -o ${CMAKE_BINARY_DIR}/docs/src/docs.tar.gz
         CONFIGURE_COMMAND ""
         BUILD_COMMAND tar -xf ${CMAKE_BINARY_DIR}/docs/src/docs.tar.gz -C <INSTALL_DIR> --strip-components=1
         INSTALL_COMMAND ""
