a48878a59db153ab98d32d41e0679eb719c5bf11
Adam Wolf <adamwolf@feelslikeburning.com>
2019-04-19 20:33:01 -0500

    Trying another boost.

--- ci/src/bootstrap.sh
+++ ci/src/bootstrap.sh
@@ -21,8 +21,8 @@ export HOMEBREW_NO_ANALYTICS=1
 echo "Installing glm 0.9.9.2"
 brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/9ce61eaa2776d0ab011e0559a86afff588f6eccb/Formula/glm.rb
 # version pinning boost
-echo "Installing boost@1.60"
-brew install boost@1.60
+echo "Installing boost"
+brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/b4de2e1ce084ec249ca61c3eff6f96617e38c081/Formula/boost.rb
 echo "Installing some dependencies"
 brew install swig glew cairo doxygen gettext wget bison libtool autoconf automake cmake
 brew install -f /vagrant/external/oce*tar.gz
