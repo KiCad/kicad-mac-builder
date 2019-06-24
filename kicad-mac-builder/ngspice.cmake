e4cbe04e2287e55865ea7e67a40ade3295d3edfe
Adam Wolf <adamwolf@feelslikeburning.com>
2019-06-24 07:14:19 -0500

    Revert "Reapplies nick's ngspice update patch. (#284)"
    This reverts commit 2e0b8c6511c2047c484a6ec17b1b182e6bf12e1e.

--- kicad-mac-builder/ngspice.cmake
+++ kicad-mac-builder/ngspice.cmake
@@ -4,7 +4,7 @@ ExternalProject_Add(
         ngspice
         PREFIX  ngspice
         GIT_REPOSITORY git://git.code.sf.net/p/ngspice/ngspice
-        GIT_TAG ngspice-30-2
+        GIT_TAG 99a20162d5038a328d335d11da69c9eee0549fdc
         UPDATE_COMMAND      ""
         PATCH_COMMAND       ""
         CONFIGURE_COMMAND  ./autogen.sh
