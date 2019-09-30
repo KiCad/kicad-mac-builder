1e9dd0a857475352e08f6a0e5e511aee7d39f4a4
Adam Wolf <adamwolf@feelslikeburning.com>
2019-09-30 16:20:08 -0500

    Ngspice debug (#299)
    * Disable debug on ngspice. Upgrade ngspice.

--- kicad-mac-builder/ngspice.cmake
+++ kicad-mac-builder/ngspice.cmake
@@ -4,11 +4,11 @@ ExternalProject_Add(
         ngspice
         PREFIX  ngspice
         GIT_REPOSITORY git://git.code.sf.net/p/ngspice/ngspice
-        GIT_TAG ngspice-30-2
+        GIT_TAG ngspice-31-2
         UPDATE_COMMAND      ""
         PATCH_COMMAND       ""
         CONFIGURE_COMMAND  ./autogen.sh
-        COMMAND  ./configure --with-ngshared --enable-xspice --enable-cider --prefix=${ngspice_INSTALL_DIR} --enable-debug=yes
+        COMMAND  ./configure --prefix=${ngspice_INSTALL_DIR} --with-ngshared --enable-xspice --enable-cider --disable-debug
         BUILD_COMMAND ${MAKE}
         BUILD_IN_SOURCE 1
         INSTALL_COMMAND make install
